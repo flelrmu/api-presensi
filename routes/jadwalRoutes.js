a19f9d122b1cee5730d81337a534e250b8dc2b72
Benni Putra Chaniago <bennichaniago2003@gmail.com>
2024-12-21 10:58:23 +0700

    update api jadwal kuliah lagi2

--- routes/jadwalRoutes.js
+++ routes/jadwalRoutes.js
@@ -11,18 +11,18 @@ const jadwalListController = require('../controllers/JadwalKuliahController');
 router.get('/jadwal', authenticate, jadwalListController.getAllJadwal);
 
 // Rute untuk melihat jadwal kuliah berdasarkan ID
-router.get('/jadwal/:id', authenticate, jadwalListController.getJadwalById);
+router.get('/jadwal/:id', jadwalListController.getJadwalById);
 
 // Rute untuk menambah jadwal kuliah baru
-router.post('/jadwal', authenticate, (req, res, next) => {
+router.post('/jadwal', (req, res, next) => {
     console.log('POST /jadwal called with data:', req.body); // Logging request body
     next();
   }, jadwalListController.createJadwal);
 
 // Rute untuk memperbarui jadwal kuliah berdasarkan ID
-router.put('/jadwal/:id', authenticate, jadwalListController.updateJadwal);
+router.put('/jadwal/:id', jadwalListController.updateJadwal);
 
 // Rute untuk menghapus jadwal kuliah berdasarkan ID
-router.delete('/jadwal/:id', authenticate, jadwalListController.deleteJadwal);
+router.delete('/jadwal/:id', jadwalListController.deleteJadwal);
 
 module.exports = router;
